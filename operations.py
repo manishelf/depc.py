3b541e5e6d8e9f6c923fd4f8fc9debf09be3dc70
manishelf <manishelf@proton.me>
2026-04-06 15:34:47 +0000

    OR operation mod

--- operations.py
+++ operations.py
@@ -1,5 +1,21 @@
 from literals import *
 
+def TRUTHY(x) -> bool:
+    if type(x) == type(True): 
+        return x
+    elif type(x) == type(1):
+        return x != 0 
+    elif type(x) == type([]):
+        res = False
+        for i in x:
+            if TRUTHY(i):
+                res = True
+                return res
+        return res
+    else:
+        print(x)
+        raise NotImplementedErrorm
+
 def NOT(x):
     if type(x) == type([]):
         result = [NOT(p) for p in x]
@@ -34,9 +50,11 @@ def OR(x, y):
         # TODO:the examples should changed to handle this
         if True or not bitwise:
             carry = 0
+            
+            MAINTAIN_STRIDE = False; 
             # process from right LSB (BIG_ENDIAN)
             for p, q in zip(reversed(x), reversed(y)):
-                if (type(p) == type([]) or type(q) == type([])) :
+                if ((type(p) == type([]) or type(q) == type([])) and not MAINTAIN_STRIDE) :
                     # recurse into nested lists
                     result.append(OR(p, q))
                 else:
@@ -45,12 +63,14 @@ def OR(x, y):
                     carry = OR(AND(p,q),AND(carry,XOR(p,q)))
                     result.append(sum_bit)
                     
-                if carry:
-                    result.append(carry)
+            if TRUTHY(carry):
+                result.append(carry)
                 
             result.reverse()
+
         else:
             result = [OR(p, q) for p, q in zip(x, y)]
+
         return result
         
     if bitwise:
@@ -69,26 +89,8 @@ def XOR(x, y):
 
 def XNOR(x, y):
     return NOT(XOR(x, y))
-    # return OR(AND(NOT(x), NOT(y)), AND(x, y)) 
-
-
-def TRUTHY(x) -> bool:
-    if type(x) == type(True): 
-        return x
-    elif type(x) == type(1):
-        return x != 0 
-    elif type(x) == type([]):
-        res = False
-        for i in x:
-            if TRUTHY(i):
-                res = True
-                return res
-        return res
-    else:
-        print(x)
-        raise NotImplementedErrorm
 
 if __name__ == "__main__":
-    x = [[1, 0]]
-    y = [[0, 1]]
+    x = [[[1, 0],[1, 1]]]
+    y = [[[1, 0],[1, 1]]]
     print(OR(x, y))
