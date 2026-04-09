7bd5c2cf73394fc1a31154c3bf7d6a3a66cba1a1
manishelf <manishelf@proton.me>
2026-04-09 16:36:04 +0000

    introduce clk

--- depc.py
+++ depc.py
@@ -26,7 +26,7 @@ class PreProcessor:
         self.file_stack = []
         self.instr_stack = []
 
-    def read(self):
+    def load(self):
         try:
             content = read_file(self.path)
             file = io.StringIO(content)
@@ -66,7 +66,7 @@ class PreProcessor:
 
     def __iter__(self):
         p = PreProcessor(self.path)
-        p.read()
+        p.load()
         return p
 
     def __next__(self):
@@ -152,143 +152,148 @@ class Processor:
             raise NotImplementedError
 
     def next_instr(self):
-        try:
-            if self.pc == len(self.instr_stack):
-                opr = self.pre_processor.__next__()
-            else:
-                opr = self.instr_stack[self.pc]
-            return opr
-        except StopIteration:
-            return ("","",())
+        if self.pc == len(self.instr_stack):
+            opr = self.pre_processor.__next__()
+        else:
+            opr = self.instr_stack[self.pc]
+        return opr
 
+    def clk(self):
+        
+        try:
+            (operation, operand, extra) = self.next_instr()
+        except StopIteration:
+            return False
+
+        if operation == "PUSH":
+            self.out_stack.append(self.literal_to_value(operand))
+
+        elif operation == "POP":
+            if len(self.out_stack) > 0:
+                self.out_stack.pop()
+
+        elif operation == "PEEK":
+            if len(self.out_stack) > 0:
+                self.out_stack.append(self.out_stack[-1])
+
+        elif operation == "STORE":
+            if len(self.out_stack) > 0:
+                top = self.out_stack.pop()
+                alias = operand
+
+                if isinstance(top, list):
+                    for i, val in enumerate(top):
+                        self.registers[alias + "_" +
+                                       str(i)] = self.value_to_literal(val)
+                else:
+                    self.registers[alias] = self.value_to_literal(top)
+
+        elif operation == "AND":
+            if len(self.out_stack) > 1:
+                x = self.out_stack.pop()
+                y = self.out_stack.pop()
+                self.out_stack.append(AND(x, y))
+
+        elif operation == "OR":
+            if len(self.out_stack) > 1:
+                x = self.out_stack.pop()
+                y = self.out_stack.pop()
+                self.out_stack.append(OR(x, y))
+
+        elif operation == "NOT":
+            if len(self.out_stack) > 0:
+                self.out_stack[-1] = NOT(self.out_stack[-1])
+
+        elif operation == "NAND":
+            if len(self.out_stack) > 1:
+                x = self.out_stack.pop()
+                y = self.out_stack.pop()
+                self.out_stack.append(NAND(x, y))
+
+        elif operation == "NOR":
+            if len(self.out_stack) > 1:
+                x = self.out_stack.pop()
+                y = self.out_stack.pop()
+                self.out_stack.append(NOR(x, y))
+
+        elif operation == "XOR":
+            if len(self.out_stack) > 1:
+                x = self.out_stack.pop()
+                y = self.out_stack.pop()
+                self.out_stack.append(XOR(x, y))
+
+        elif operation == "XNOR":
+            if len(self.out_stack) > 1:
+                x = self.out_stack.pop()
+                y = self.out_stack.pop()
+                self.out_stack.append(XNOR(x, y))
+
+        elif operation == "IF":
+            if len(self.out_stack) > 0:
+                top = self.out_stack.pop()
+
+                if not TRUTHY(top):
+                    self.conditional_skip_depth = 1
+                    # this will block CLK until if_end recieved
+                    while self.conditional_skip_depth > 0:
+                        self.pc += 1
+                        try:
+                            (op, _, _) = self.next_instr()
+                        except StopIteration:
+                            return False
+                        if op == "IF":
+                            self.conditional_skip_depth += 1
+                        elif op == "IF_END":
+                            self.conditional_skip_depth -= 1
+                    return True
+
+        elif operation == "IF_END":
+            pass
+
+        elif operation == "LABEL":
+            self.labels[operand] = self.pc
+
+        elif operation == "GOTO":
+            self.pc = self.labels[operand]
+
+        elif operation == "DONE":
+            return False
+
+        elif operation == "SEQUENCE":
+            self.sequence_stack.append(operand)
+            self.sequence_item_counter_stack.append(0)
+
+        elif operation == "SEQUENCE_END":
+            if len(self.out_stack) > 1 and len(self.sequence_item_counter_stack) > 0:
+                self.sequence_stack.pop()
+                self.sequence_item_counter_stack.pop()
+
+        elif operation == "CLEAN":
+            self.out_stack = []
+            # self.registers = dict()
+
+        elif operation == "SHOW":
+            for val in self.out_stack:
+                print(val)
+
+        if len(self.sequence_stack) > 0 and len(self.out_stack) > 0:
+            top = self.out_stack.pop()
+            sequence_name = self.sequence_stack[-1]
+            item_index = self.sequence_item_counter_stack[-1]
+
+            item_name = sequence_name + "_" + str(item_index)
+            self.sequence_item_counter_stack[-1] = item_index + 1
+
+            self.registers[item_name] = self.value_to_literal(top)
+
+        self.pc += 1
+        return True
 
     def out(self):
-
-        while self.pc <= len(self.instr_stack):
-
-            (operation, operand, extra) = self.next_instr()
-
-            if operation == "PUSH":
-                self.out_stack.append(self.literal_to_value(operand))
-
-            elif operation == "POP":
-                if len(self.out_stack) > 0:
-                    self.out_stack.pop()
-
-            elif operation == "PEEK":
-                if len(self.out_stack) > 0:
-                    self.out_stack.append(self.out_stack[-1])
-
-            elif operation == "STORE":
-                if len(self.out_stack) > 0:
-                    top = self.out_stack.pop()
-                    alias = operand
-
-                    if isinstance(top, list):
-                        for i, val in enumerate(top):
-                            self.registers[alias + "_" +
-                                           str(i)] = self.value_to_literal(val)
-                    else:
-                        self.registers[alias] = self.value_to_literal(top)
-
-            elif operation == "AND":
-                if len(self.out_stack) > 1:
-                    x = self.out_stack.pop()
-                    y = self.out_stack.pop()
-                    self.out_stack.append(AND(x, y))
-
-            elif operation == "OR":
-                if len(self.out_stack) > 1:
-                    x = self.out_stack.pop()
-                    y = self.out_stack.pop()
-                    self.out_stack.append(OR(x, y))
-
-            elif operation == "NOT":
-                if len(self.out_stack) > 0:
-                    self.out_stack[-1] = NOT(self.out_stack[-1])
-
-            elif operation == "NAND":
-                if len(self.out_stack) > 1:
-                    x = self.out_stack.pop()
-                    y = self.out_stack.pop()
-                    self.out_stack.append(NAND(x, y))
-
-            elif operation == "NOR":
-                if len(self.out_stack) > 1:
-                    x = self.out_stack.pop()
-                    y = self.out_stack.pop()
-                    self.out_stack.append(NOR(x, y))
-
-            elif operation == "XOR":
-                if len(self.out_stack) > 1:
-                    x = self.out_stack.pop()
-                    y = self.out_stack.pop()
-                    self.out_stack.append(XOR(x, y))
-
-            elif operation == "XNOR":
-                if len(self.out_stack) > 1:
-                    x = self.out_stack.pop()
-                    y = self.out_stack.pop()
-                    self.out_stack.append(XNOR(x, y))
-
-            elif operation == "IF":
-                if len(self.out_stack) > 0:
-                    top = self.out_stack.pop()
-
-                    #TODO: this is currently breaking as look ahead is removed for REPL
-                    if not TRUTHY(top):
-                        self.conditional_skip_depth = 1
-                        while self.conditional_skip_depth > 0:
-                            self.pc += 1
-                            (op, _, _) = self.next_instr()
-                            if op == "IF":
-                                self.conditional_skip_depth += 1
-                            elif op == "IF_END":
-                                self.conditional_skip_depth -= 1
-                        continue
-
-            elif operation == "IF_END":
-                pass
-
-            elif operation == "LABEL":
-                self.labels[operand] = self.pc
-
-            elif operation == "GOTO":
-                self.pc = self.labels[operand]
-                continue
-
-            elif operation == "DONE":
-                break
-
-            elif operation == "SEQUENCE":
-                self.sequence_stack.append(operand)
-                self.sequence_item_counter_stack.append(0)
-
-            elif operation == "SEQUENCE_END":
-                if len(self.out_stack) > 1 and len(self.sequence_item_counter_stack) > 0:
-                    self.sequence_stack.pop()
-                    self.sequence_item_counter_stack.pop()
-
-            elif operation == "CLEAN":
-                self.out_stack = []
-                # self.registers = dict()
-
-            elif operation == "SHOW":
-                for val in self.out_stack:
-                    print(val)
-
-            if len(self.sequence_stack) > 0 and len(self.out_stack) > 0:
-                top = self.out_stack.pop()
-                sequence_name = self.sequence_stack[-1]
-                item_index = self.sequence_item_counter_stack[-1]
-
-                item_name = sequence_name + "_" + str(item_index)
-                self.sequence_item_counter_stack[-1] = item_index + 1
-
-                self.registers[item_name] = self.value_to_literal(top)
-
-            self.pc += 1
-
+        self.pre_processor.load()
+        running = True
+        while running:
+            running = self.clk()
         return self.out_stack
 
 class Repl:
@@ -305,6 +310,9 @@ class Repl:
     # duck typing
     def readline(self):
         return input(">>>> ")
+    
+    def load(self):
+        print("Hello, welcome to Depc , checkout function.md to see available functions")
 
     def __next__(self):
         try:
--- main.py
+++ main.py
@@ -4,12 +4,10 @@ import depc
 
 def main():
     if len(sys.argv) < 2:
-        print("Hello, welcome to Depc , checkout function.md to see available functions")
         processor = depc.Processor(depc.Repl())
     else:
         print("processing ", sys.argv[1])
         pre_processor = depc.PreProcessor(sys.argv[1])
-        pre_processor.read()
         processor = depc.Processor(pre_processor)
 
         for row in pre_processor:
